b112788e1afea90f3147dd12db4da4594258e26c
kranthi1710 <67594544+kranthi1710@users.noreply.github.com>
2026-04-09 02:14:15 +0530

    Time: 1 ms (74.23%) | Memory: 55.9 MB (49.94%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,6 +3,20 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    console.log(x**0.5);
-  return Math.floor(x**0.5);
+    if(x<2) return x; 
+let left=1;
+let right=Math.floor(x/2);
+
+
+  while(left<=right){
+    let mid=Math.floor((left+right)/2);
+     if(x===mid*mid){
+       return mid;
+     } else if(x>mid*mid){
+        left=mid+1;
+     } else {
+        right=mid-1
+     }
+  }
+  return right;
 };
